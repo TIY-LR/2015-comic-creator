87660fc57028f03813e0ffa00874d22a98b070bf
Brandi Soucy <bbsoucy@gmail.com>
2015-12-05 20:34:03 -0600

    Allow adding new tile

--- app/projects/tiles/route.js
+++ app/projects/tiles/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  tileService: Ember.inject.service('projects.tiles'),
+
+  actions: {
+    addTile(project) {
+      var tileService = this.get('tileService');
+
+      if (this.get('tileService').currentlyEditingNew()) {
+        return;
+      }
+
+      var tile = this.store.createRecord('tile');
+
+      tileService.set('currentlyEditing', tile);
+
+      project.get('tiles').pushObject(tile);
+    },
+  },
 });
--- app/projects/tiles/service.js
+++ app/projects/tiles/service.js
@@ -2,4 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   currentlyEditing: null,
+
+  currentlyEditingNew() {
+    return this.get('currentlyEditing.isNew');
+  },
 });
