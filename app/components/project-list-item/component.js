30417e1466a614793b1d2db073b6563e00b4a606
Brandi Soucy <bbsoucy@gmail.com>
2015-12-06 10:06:50 -0600

    allowing delete project

--- app/components/project-list-item/component.js
+++ app/components/project-list-item/component.js
@@ -4,4 +4,10 @@ export default Ember.Component.extend({
   tagName: 'tile',
 
   classNames: ['mdl-cell mdl-cell--4-col mdl-cell--2-col-phone'],
+
+  actions: {
+    delete(tile) {
+      tile.destroyRecord();
+    },
+  },
 });
